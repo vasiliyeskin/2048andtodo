52536008aa3bafbc6a4dad2a52ef0036acda06ce
vasiliyeskin <eskin@rf.unn.ru>
2017-11-15 19:56:39 -0600

    Make to war deploy on tomcat

--- src/main/java/com/example/big_appls/BigApplsApplication.java
+++ src/main/java/com/example/big_appls/BigApplsApplication.java
@@ -2,11 +2,23 @@ package com.example.big_appls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class BigApplsApplication {
+public class BigApplsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BigApplsApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BigApplsApplication.class, args);
 	}
+
+/*	public static void main(String[] args) {
+		SpringApplication.run(BigApplsApplication.class, args);
+	}*/
 }
